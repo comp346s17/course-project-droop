c616bc1aaefa97be118fbdb2ef52fe31be237c32
stevenroach7 <sroach@macalester.edu>
2017-04-18 16:42:01 -0500

    Sets up basic routes

--- droopproject/main/static/main/app.js
+++ droopproject/main/static/main/app.js
@@ -5,6 +5,15 @@ droopApp.config(function($routeProvider, $httpProvider, $resourceProvider) {
     when('/', {
       template: '<home></home>'
     }).
+    when('/canvas', {
+      template: '<drawing-canvas></drawing-canvas>'
+    }).
+    when('/gallery', {
+      template: '<drawing-gallery></drawing-gallery>'
+    }).
+    when('/gallery/:id', {
+      template: '<drawing-detail></drawing-detail>'
+    }).
     otherwise('/');
 
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
@@ -15,6 +24,29 @@ droopApp.config(function($routeProvider, $httpProvider, $resourceProvider) {
 droopApp.component('home', {
   templateUrl: 'static/main/home.template.html',
   controller: function($scope) {
+    }
+});
 
+droopApp.component('drawingCanvas', {
+  templateUrl: 'static/main/canvas.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('drawingDetail', {
+  templateUrl: 'static/main/drawing-detail.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('drawingGallery', {
+  templateUrl: 'static/main/gallery.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('droopNavbar', {
+  templateUrl: 'static/main/navbar.template.html',
+  controller: function($scope) {
     }
 });
