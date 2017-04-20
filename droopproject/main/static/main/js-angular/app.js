189b301115d8cc60eae9781891bfc2716b31b8d4
stevenroach7 <sroach@macalester.edu>
2017-04-19 23:09:08 -0500

    Separates angularJs files

--- droopproject/main/static/main/js-angular/app.js
+++ droopproject/main/static/main/js-angular/app.js
@@ -20,33 +20,3 @@ droopApp.config(function($routeProvider, $httpProvider, $resourceProvider) {
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     $resourceProvider.defaults.stripTrailingSlashes = false;
 });
-
-droopApp.component('home', {
-  templateUrl: 'static/main/templates/home.template.html',
-  controller: function($scope) {
-    }
-});
-
-droopApp.component('drawingCanvas', {
-  templateUrl: 'static/main/templates/canvas.template.html',
-  controller: function($scope) {
-    }
-});
-
-droopApp.component('drawingDetail', {
-  templateUrl: 'static/main/templates/drawing-detail.template.html',
-  controller: function($scope) {
-    }
-});
-
-droopApp.component('drawingGallery', {
-  templateUrl: 'static/main/templates/gallery.template.html',
-  controller: function($scope) {
-    }
-});
-
-droopApp.component('droopNavbar', {
-  templateUrl: 'static/main/templates/navbar.template.html',
-  controller: function($scope) {
-    }
-});
---
+++ droopproject/main/static/main/js-angular/controllers.js
@@ -0,0 +1,31 @@
+angular.module('droopApp.controllers', [])
+
+droopApp.component('home', {
+  templateUrl: 'static/main/templates/home.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('drawingCanvas', {
+  templateUrl: 'static/main/templates/canvas.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('drawingDetail', {
+  templateUrl: 'static/main/templates/drawing-detail.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('drawingGallery', {
+  templateUrl: 'static/main/templates/gallery.template.html',
+  controller: function($scope) {
+    }
+});
+
+droopApp.component('droopNavbar', {
+  templateUrl: 'static/main/templates/navbar.template.html',
+  controller: function($scope) {
+    }
+});
