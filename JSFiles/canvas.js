04b86ba1d5e47dafd0c4795b55d6a01ef2f62de5
Dochoa1 <dochoa@macalester.edu>
2017-04-19 20:37:16 -0500

    Refactors color and linesize changes

--- JSFiles/canvas.js
+++ JSFiles/canvas.js
@@ -1,108 +1,133 @@
 var context = $("#canvas").get(0).getContext("2d");
-var paint;
+context.canvas.width = window.innerWidth * .9;
+context.canvas.height = window.innerHeight * .75;
+var painting;
 
-var colorPurple = "#cb3594";
-var colorGreen = "#659b41";
-var colorYellow = "#ffcf33";
-var colorBrown = "#986928";
-
-var curColor = colorPurple;
+// Keeping track of latest used colors,  sizes, tools, locations
+//Colors
+var curColor = "black"
 var clickColor = new Array();
 
-var clickTool = new Array();
-var curTool = "marker";
+//Line sizes
+var lineSizes = {"small": 2, "regular": 5, "large": 10, "extraLarge": 15, "bigBertha": 30}
+var curLineSize = lineSizes["regular"]
+var clickSize = new Array();
 
+//Tools
+var curTool = "marker";
+var clickTool = new Array();
+
+//Click locations
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
+//
 
 $('#clear').click(function() {
+    //Clears the canvas & reset arrays
+
     context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
     clickX = [];
     clickY = [];
     clickDrag = [];
     clickColor = [];
+    clickSize = [];
+    curTool = "marker"
 });
 
 //Changing all the colors
-$('#purple').click(function(){
-    curColor = colorPurple;
-});
-$('#green').click(function(){
-    curColor = colorGreen;
-});
-$('#yellow').click(function(){
-    curColor = colorYellow;
-});
-$('#brown').click(function(){
-    curColor = colorBrown;
-});
+$(".color").click(function() {
+    curColor = this.id //Using built in Javascript colors
+})
+//
 
+//Changing all the line Sizes, Refactor into one function where each button shares a class 'draw-size'
+$(".draw-size").click(function() {
+    curLineSize = lineSizes[this.id]
+})
+//
+
+//Changing the tools
 $(".color").click(function(){
     if (curTool == "eraser"){
         curTool = "marker"
     }
 });
-
-//Changing the tools
 $('#marker').click(function(){
     curTool = "marker";
 });
 $('#eraser').click(function(){
     curTool = "eraser";
 });
+//
+
+$("#btnSave").bind("click", function () {
+    //Save a copy of the canvas to a hidden image file
+
+    var copyOfCanvas = $('#canvas')[0].toDataURL();
+    $("#img").attr("src", copyOfCanvas);
+    //$("#img").show();
+})
 
 function addClick(x, y, dragging){
-  clickX.push(x);
-  clickY.push(y);
-  clickDrag.push(dragging);
-  if(curTool == "eraser"){
-    clickColor.push("white");
-  } else {
-    clickColor.push(curColor);
-  }
+    //Called whenever the user clicks on the canvas adds x,y,drag and pushed color or white if eraser
+
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+    if(curTool == "eraser"){
+        clickColor.push("white");
+    } else {
+        clickColor.push(curColor);
+    }
+    clickSize.push(curLineSize);
 }
 
+/*
+    offSet{Direction} returns the {Direction} position in pixels relative to the {Direction} side of the parent element.
+    page{Coordinate axis} returns the position of the mouse pointer
+*/
 $('#canvas').mousedown(function(e){
-  paint = true;
-  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-  redraw();
+    //Adds initial painting if the user is drawing on the canvas
+
+    painting = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
 });
 
 $('#canvas').mousemove(function(e){
-  if(paint){
+  if(painting){
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
     redraw();
   }
 });
 
 $('#canvas').mouseup(function(e){
-  paint = false;
+  painting = false;
 });
 
 $('#canvas').mouseleave(function(e){
-  paint = false;
+  painting = false;
 });
 
 function redraw(){
-    //Currently very inefficent, redraws canvas over and over
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+//Currently very inefficent, redraws canvas over and over
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-  context.strokeStyle = "#df4b26";
-  context.lineJoin = "round";
-  context.lineWidth = 5;
+    context.lineJoin = "round";
 
-  for(var i=0; i < clickX.length; i++) {
-    context.beginPath();
-    if(clickDrag[i] && i){
-      context.moveTo(clickX[i-1], clickY[i-1]);
-     }else{
-       context.moveTo(clickX[i]-1, clickY[i]);
-     }
-     context.lineTo(clickX[i], clickY[i]);
-     context.closePath();
-     context.strokeStyle = clickColor[i];
-     context.stroke();
+    for(var i=0; i < clickX.length; i++) {
+        context.beginPath();
+        if(clickDrag[i] && i){
+            context.moveTo(clickX[i-1], clickY[i-1]);
+        }else{
+            context.moveTo(clickX[i]-1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.strokeStyle = clickColor[i];
+        context.lineWidth = clickSize[i]
+        context.stroke();
     }
     context.globalAlpha = 1;
 }
