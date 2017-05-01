cd9037136c61e5fb7c27371428bbe174059f5a44
Dochoa1 <dochoa@macalester.edu>
2017-04-30 19:03:39 -0500

    Fixes window size on new drawing

--- droopproject/main/static/main/canvas.js
+++ droopproject/main/static/main/canvas.js
@@ -12,7 +12,8 @@ context3.canvas.height = window.innerHeight * 0.6;
 var img = $("#img");
 
 img.on('load', function() {
-
+    $(this).css("width", context.canvas.width);
+    $(this).css("height", context.canvas.height);
     $("#canvas").css("background-image", "url(" + $(this).data("root") + ")");
     $("#canvas").css("background", "no-repeat center center cover"); //Background window-size messed up
     $("#canvas3").css("background-image", "url(" + $(this).data("root") + ")");
